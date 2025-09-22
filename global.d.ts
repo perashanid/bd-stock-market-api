e8da9b1468df132b5f8f9fc18e5fb83e174dc3f3
perashanid <shanidsajjatuzislamrabid@gmail.com>
2025-09-22 22:20:53 +0600

    fix

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,36 @@
-/// <reference types="node" />
-
+// Global Node.js type declarations
 declare global {
-  var process: NodeJS.Process;
-  var console: Console;
-  var Buffer: BufferConstructor;
+  var process: {
+    env: { [key: string]: string | undefined };
+    exit(code?: number): never;
+    uptime(): number;
+    on(event: string, listener: (...args: any[]) => void): any;
+  };
+  
+  var console: {
+    log(...args: any[]): void;
+    error(...args: any[]): void;
+    warn(...args: any[]): void;
+    info(...args: any[]): void;
+  };
+  
+  var Buffer: {
+    from(data: any): any;
+    alloc(size: number): any;
+  };
+  
   var __dirname: string;
   var __filename: string;
-  var global: NodeJS.Global;
+  var global: any;
+  
+  class URLSearchParams {
+    constructor(init?: string | string[][] | Record<string, string>);
+    append(name: string, value: string): void;
+    delete(name: string): void;
+    get(name: string): string | null;
+    set(name: string, value: string): void;
+    toString(): string;
+  }
 }
 
 export {};
